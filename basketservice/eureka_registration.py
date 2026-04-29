5589964cb78219653c697bb15478a179ce7aa0de
Damia <dsargent@student.neumont.edu>
2026-04-28 23:10:46 -0600

    Set-up both catalog and basket service with Eureka

---
+++ basketservice/eureka_registration.py
@@ -0,0 +1,27 @@
+import os
+import socket
+import py_eureka_client.eureka_client as eureka_client
+
+_client_inited = False
+
+async def register_with_eureka():
+    global _client_inited
+    if _client_inited:
+        return
+
+    eureka_server = os.getenv("EUREKA_SERVER", "http://SEN300EurekaServer:8761/eureka/")
+    app_name = os.getenv("EUREKA_APP_NAME", "basketservice")
+    instance_host = os.getenv("EUREKA_INSTANCE_HOST", socket.gethostname())
+    instance_port = int(os.getenv("EUREKA_INSTANCE_PORT", "8000"))
+    health_path = os.getenv("EUREKA_HEALTH_PATH", "/health")
+
+    await eureka_client.init_async(
+        eureka_server=eureka_server,
+        app_name=app_name,
+        instance_host=instance_host,
+        instance_port=instance_port,
+        health_check_url=f"http://{instance_host}:{instance_port}{health_path}",
+        home_page_url=f"http://{instance_host}:{instance_port}/"
+    )
+
+    _client_inited = True
